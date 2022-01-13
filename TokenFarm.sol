cca35471c4ddfa0b53c21490a9a2370af2faab0b
AkshatGada <96907514+AkshatGada@users.noreply.github.com>
2022-01-13 23:46:58 +0530

    Add files via upload

---
+++ TokenFarm.sol
@@ -0,0 +1,59 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.21 <0.6.0;
+
+import"./DappToken.sol";
+import"./DaiToken.sol";
+
+contract TokenFarm{
+    string public name = "Dapp Token Farm";
+    DappToken public dappToken;
+    DaiToken public daiToken;
+    address public owner;
+
+    address[] public stakers;
+    mapping(address => uint) public stakingBalance;
+    mapping(address => bool) public hasStaked;
+    mapping(address => bool) public isStaking;
+
+
+    constructor(DappToken _dappToken, DaiToken _daiToken ) public{
+        dappToken = _dappToken;
+        daiToken = _daiToken;
+     owner = msg.sender;
+ }
+
+ function stakeTokens(uint _amount) public {
+
+     require(_amount > 0,'No balacne in your account');
+      daiToken.transferFrom(msg.sender,address(this),_amount);
+      stakingBalance[msg.sender] = stakingBalance[msg.sender] + _amount;
+
+      if(!hasStaked[msg.sender]){
+          stakers.push(msg.sender);
+      }
+
+      isStaking[msg.sender] = true;
+      hasStaked[msg.sender] = true;
+    }
+
+    function unstakeTokens() public{
+        uint balance = stakingBalance[msg.sender];
+        require(balance > 0 , 'Amount should be greater than zero');
+        daiToken.transfer(msg.sender,balance);
+        stakingBalance[msg.sender] = 0;
+        isStaking[msg.sender] = false;
+    }
+
+    function issueTokens() public{
+        require(msg.sender== owner,'Caller must be the owner');
+        for(uint i = 0; i < stakers.length ; i++){
+            address recepient = stakers[i];
+            uint balance = stakingBalance[recepient];
+            if(balance > 0){
+                dappToken.transfer(recepient,balance);
+
+            }
+        }
+
+    }
+}
